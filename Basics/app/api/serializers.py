682a843c0aa07a81c25059f6eb6aa534f4822eb3
divyansh-tamrakar <76593080+divyansh-tamrakar@users.noreply.github.com>
2022-01-22 21:36:35 +0530

    Delete Basics directory

--- Basics/app/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from app import models
-from rest_framework.serializers import ModelSerializer
-
-
-class RoomSerializer(ModelSerializer):
-
-    class Meta:
-        model = models.Room
-        fields = "__all__"
